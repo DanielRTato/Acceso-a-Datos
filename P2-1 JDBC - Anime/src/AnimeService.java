1d4a3943a7a2886fba10690813d02f6b026c5cdf
Daniel Rodriguez <drodrigueztato@danielcastelao.org>
2025-09-29 12:51:11 +0200

    Agregar métodos para leer, actualizar y eliminar animes en AnimeService

--- P2-1 JDBC - Anime/src/AnimeService.java
+++ P2-1 JDBC - Anime/src/AnimeService.java
@@ -1,12 +1,13 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnimeService {
+
     public void insertAnime(Anime anime) {
-        String sql = "insert into anime (nome, descripcion, data, puntuacion) values (?, ?, ?, ?)";
+        String consulta = "insert into anime (nome, descripcion, data, puntuacion) values (?, ?, ?, ?)";
         try (Connection conn = Conexion.conexion();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+             PreparedStatement stmt = conn.prepareStatement(consulta)) {
 
             stmt.setString(1, anime.getNome());
             stmt.setString(2, anime.getDescripcion());
@@ -14,9 +15,60 @@ public class AnimeService {
             stmt.setInt(4, anime.getPuntuacion());
 
             stmt.executeUpdate();
-            System.out.println("✅ Inserido: " + anime.getNome());
+            System.out.println("Inserido: " + anime.getNome());
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
+    public List<Anime> leer() {
+        List<Anime> lista = new ArrayList<>();
+        String consulta = "select * from anime";
+        try (Connection conn = Conexion.conexion();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(consulta)) {
+
+            while (rs.next()) {
+                Anime anime = new Anime(
+                        rs.getString("nome"),
+                        rs.getString("descripcion"),
+                        rs.getDate("data"),
+                        rs.getInt("puntuacion")
+                );
+                lista.add(anime);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+    public Anime leerFiltrado () {
+        return null;
+
+    }
+
+    public void actualizar() {
+
+    }
+
+    public void eliminar (String nome) {
+        String consulta = "delete from anime where nome = ?";
+        try (Connection conn = Conexion.conexion();
+             PreparedStatement stmt = conn.prepareStatement(consulta)) {
+
+            stmt.setString(1, nome);
+            int filas = stmt.executeUpdate();
+
+            if (filas > 0) {
+                System.out.println("Eliminado " + nome);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
 }
