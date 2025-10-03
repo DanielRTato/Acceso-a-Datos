da6a95b26098473d4e9634c7a2e8cffd617919ba
Daniel Rodriguez <drodrigueztato@danielcastelao.org>
2025-10-03 14:27:59 +0200

    Parte 2 terminada

--- Practica1-3_I_O_SerializacioneXML/src/Main.java
+++ Practica1-3_I_O_SerializacioneXML/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Producto producto1 = new Producto("manzana", 1, 2.3);
+        Producto producto1 = new Producto("Parte1", 1, 2.3);
         Producto productoValeiro = new Producto();
 
         producto1.guardarObjecto("serial");
@@ -10,6 +10,16 @@ public class Main {
 
         System.out.println(producto1);
         System.out.println(productoValeiro);
+
+        productoTransient producto2 = new productoTransient("Parte2", 11 , 5.2);
+        productoTransient productoValeiro2 = new productoTransient();
+
+        producto2.guardarObjecto("serial2");
+        productoValeiro2.escribirObjeto("serial2");
+
+        System.out.println(producto2);
+        System.out.println(productoValeiro2);
+
 
 
     }
--- Practica1-3_I_O_SerializacioneXML/src/productoTransient.java
+++ Practica1-3_I_O_SerializacioneXML/src/productoTransient.java
@@ -1,7 +1,54 @@
-import java.io.Serializable;
+import java.io.*;
 
 public class productoTransient implements Serializable {
     String nome;
     transient int num1;
     double num2;
+
+    public productoTransient(String nome, int num1, double num2) {
+        this.nome = nome;
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public productoTransient(){
+
+    }
+
+
+    public void guardarObjecto(String ruta) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta));
+            oos.writeObject(this);
+            System.out.println("Guardado");
+        } catch (IOException | RuntimeException e) {
+            System.out.println("Erroral guardad:  " + e);;
+        }
+    }
+
+
+    public void escribirObjeto(String ruta) {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ruta));
+            productoTransient producto = (productoTransient) ois.readObject();
+            this.nome = producto.nome;
+            this.num1 = producto.num1;
+            this.num2 = producto.num2;
+            System.out.println("Cargado bien");
+
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error al escribir: " + e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "productoTransient{" +
+                "nome='" + nome + '\'' +
+                ", num1=" + num1 +
+                ", num2=" + num2 +
+                '}';
+    }
 }
+
+
