92e15c3155739a11b76ffb143a831b3a2a6eefea
DanielRTato <drodrigueztato@danielcastelao.org>
2025-10-20 20:14:55 +0200

    Serializar vehiculo

---
+++ Semu2/src/ReadWrite/Serializar.java
@@ -0,0 +1,45 @@
+package ReadWrite;
+
+import DAO.Conexion;
+import model.Vehiculo;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Serializar {
+
+    public static void serializarVehiculo (String ruta) {
+        List<Vehiculo> listVehiculo = new ArrayList<>();
+        String sql = "select * from vehiculo";
+
+        try (Connection conn = Conexion.conexion();
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Vehiculo vehiculo = new Vehiculo(
+                        rs.getInt("id"),
+                        rs.getString("modelo"),
+                        rs.getString("marca"),
+                        rs.getInt("ano"),
+                        rs.getString("descripcion")
+                );
+                listVehiculo.add(vehiculo);
+            }
+            ObjectOutput oos = new ObjectOutputStream(new FileOutputStream(ruta));
+            oos.writeObject(listVehiculo);
+            System.out.println("Serialización completada con exito en: " + ruta);
+
+        } catch (SQLException | IOException e) {
+            System.out.println("Error al serializar las Vehiculos: " + e.getMessage());
+        }
+    }
+}
